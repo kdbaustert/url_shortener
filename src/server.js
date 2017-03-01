e5af02ff13bdff9894ad280add469624d52e2bd7
Kenny Baustert <kdbaustert@fullsail.edu>
2017-03-01 18:04:46 -0500

    Made changes to server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 //configuration
 //set the port to a variable, defaults at 3000
  const port = process.env.PORT || 3000;
-//export server to use with other modules
+//export server
 exports.server = app.listen(port, () => {
-  console.log('Server is using', port);
+  console.log('Server Active On', port);
 });
