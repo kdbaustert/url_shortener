9290ed53d68408a24bc3954a7b29d582214652a5
Kenny Baustert <kdbaustert@fullsail.edu>
2017-03-02 20:28:37 -0500

    created my env and finished my api url

---
+++ src/routes/api/url.js
@@ -0,0 +1,60 @@
+// require url
+const url = require('../../models/url');
+const genShortenUrl = require('../../modules/short_url');
+
+module.exports = (express) => {
+  // Express router
+  const router = express.Router();
+
+  // Create
+  router.post('/url', (req, res) => {
+    req.body.shortened_url = genShortenUrl.genShortenUrl();
+    url.create(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Get all
+  router.get('/url', (req, res) => {
+    url.findAll((err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Get by id
+  router.get('/url/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.find(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Update
+  router.get('/url/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.update(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Delete
+  router.delete('/url/:id', (req, res) => {
+    req.body.id = req.params.id;
+    url.destroy(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  // Return
+  return router;
+};
